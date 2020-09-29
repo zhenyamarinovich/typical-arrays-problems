7299de4fa6d4b4fc8ee070e6d0abdce39ee7be28
Evgeniy Marinovich <zmarinovich@mail.ru>
2020-09-29 08:01:06 +0300

    feat: solve typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+};
